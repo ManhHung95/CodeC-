76887fe7c9540b17332c4ea79cb19480cb336aa9
ManhHung95 <hungd13cn7ptit@gmail.com>
2016-04-05 23:18:56 +0700

    To sua roi nhe cai dit me cau

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int gt(int n){
+	if (n==0) return 1;
+	else return n*gt(n-1);
 }
-main(){
+int main(){
+	int n;
+	cout<<"Nhap vao n: "; 
+	cin>>n;
+	cout<<n<<"! = "<<gt(n);
+	return 0;
 }
